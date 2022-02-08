c60940c7e6eeb46ba9ee11eb598d4eb3a01ea957
simons1008 <69195892+simons1008@users.noreply.github.com>
2022-02-08 21:44:40 +0100

    Add files via upload

--- SW/OOP/Tag_10/Some_Shapes.py
+++ SW/OOP/Tag_10/Some_Shapes.py
@@ -1,1 +1,42 @@
+import pygame, sys
+from pygame.locals import *
 
+# Initialize program
+pygame.init()
+
+# Assign FPS a value
+FPS = 30
+FramePerSec = pygame.time.Clock()
+
+# Setting up color objects
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Setup a 300x300 pixel display with caption
+DISPLAYSURF = pygame.display.set_mode((300,300))
+DISPLAYSURF.fill(WHITE)
+pygame.display.set_caption("Example")
+
+# Creating Lines and Shapes
+pygame.draw.line(DISPLAYSURF, BLUE, (150,130), (130,170))
+pygame.draw.line(DISPLAYSURF, BLUE, (150,130), (170,170))
+pygame.draw.line(DISPLAYSURF, GREEN, (130,170), (170,170))
+pygame.draw.circle(DISPLAYSURF, BLACK, (100,50), 30)
+pygame.draw.circle(DISPLAYSURF, BLACK, (200,50), 30)
+pygame.draw.rect(DISPLAYSURF, RED, (100, 200, 100, 50), 2)
+pygame.draw.rect(DISPLAYSURF, BLACK, (110, 260, 80, 5))
+
+# Beginning Game Loop. The Game Loop is purely for show.
+# In a program with no events that can occur, we don’t need a game loop.
+# But it’s just there to show what one looks like when we use it later. 
+while True:
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    FramePerSec.tick(FPS)
