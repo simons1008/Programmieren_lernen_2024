801b4ae27876973c7a11c8661e6cd8aa9260bf56
simons1008 <martin.simons@tdmail.de>
2022-02-15 16:55:46 +0100

    Obsolete Dateien entfernt

--- SW/Lehrer/Fakultaet.py
+++
@@ -1,6 +0,0 @@
-# Funktion berechnet rekursiv die Fakultät von x 
-def factorial(x:int) -> int:
-    if x <= 1:
-        return 1
-
-    return x * factorial(x - 1)
--- SW/Lehrer/potenzen.py
+++
@@ -1,7 +0,0 @@
-# Potenzen berechnen: 2. Potenz und 3. Potenz des Eingabewerts
-
-# Funktion mit Datentyp
-def potenzen(zahl: float) -> list[float]:
-    quadratzahl = zahl * zahl
-    kubikzahl = quadratzahl * zahl
-    return [quadratzahl, kubikzahl]
