aca9b644d92683688fb7664f850e820409e0ce37
simons1008 <martin.simons@tdmail.de>
2022-09-04 00:07:49 +0200

    Datei verschoben

---
+++ SW/5_Benutzereingaben/Quadrat_ausgeben.py
@@ -0,0 +1,10 @@
+# Das Quadrat der eingegebenen Zahl ausgeben
+
+# Benutzereingabe anfordern
+zahl = input("Gib eine Zahl ein ")
+
+# String in Dezimalzahl umwandeln
+zahl = float(zahl)
+
+# Quadrat ausgeben
+print("Das Quadrat der eingegebenen Zahl ist ", zahl *  zahl)
