c78ca383f308cfb56ba505c0e4bf9408330d9c79
simons1008 <martin.simons@tdmail.de>
2022-09-03 23:21:18 +0200

    Datei verschoben

---
+++ SW/Rechnen_mit_Zeichenketten_und_Zahlen/Kurve_ohne_Grafik.py
@@ -0,0 +1,10 @@
+print( 'Ausgabe Kurve ohne Grafik' );
+print( 3 * '*' );
+print( 5 * '*' );
+print( 8 * '*' );
+print( 9 * '*' );
+print( 10 * '*' );
+print( 9 * '*' );
+print( 8 * '*' );
+print( 5 * '*' );
+print( 3 * '*' );
