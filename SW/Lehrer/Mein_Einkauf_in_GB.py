58ab3dc839a2bcebfe9bf3c57c3d2212d23ea770
simons1008 <martin.simons@tdmail.de>
2022-11-14 03:52:06 +0100

    Funktion korrigiert

--- SW/Lehrer/Mein_Einkauf_in_GB.py
+++ SW/Lehrer/Mein_Einkauf_in_GB.py
@@ -1,14 +1,16 @@
 # Aufgabe der Funktion ist die Addition aller Einkäufe aus einer Liste
+# und die Umrechnung der Summe in EUR (1 GBP = 1,21 EUR)
 
 # Input der Funktion ist der Kassenzettel mit den Preisen in GBP
-# Output der Funktion ist die Summe der Preise in GBP
+# Output der Funktion ist die Summe der Preise in EUR
 
 # Funktion mit Datentyp
 def summiere(kassenzettel: list[float]) -> float:
     summe = 0
     for x in kassenzettel:
         summe += x
-    return summe
+    eur = summe * 1.21
+    return eur
 
 # Input und Output initialisieren
 mein_kassenzettel = [9.0, 11.99, 12.99, 24.99]
@@ -20,16 +22,5 @@ alle_einkaeufe = summiere(mein_kassenzettel)
 ohne_weste = summiere(mein_kassenzettel[0:3])
 
 # Ergebnisse drucken
-print("Alle Einkäufe") 
-for i in range(len(mein_kassenzettel)):
-    print("{:5.2f}".format(mein_kassenzettel[i]))
-print("Summe")
-print("{:5.2f}".format(alle_einkaeufe))
-
-print("")
-
-print("Ohne Weste") 
-for i in range(len(mein_kassenzettel[0:3])):
-    print("{:5.2f}".format(mein_kassenzettel[i]))
-print("Summe")
-print("{:5.2f}".format(ohne_weste))
+print("Alle Käufe EUR: {:5.2f}".format(alle_einkaeufe))
+print("Ohne Weste EUR: {:5.2f}".format(ohne_weste))
