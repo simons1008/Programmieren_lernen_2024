e1456340107f2b3555b226fdd085661060f98438
simons1008 <martin.simons@tdmail.de>
2022-11-14 04:06:13 +0100

    Vorlage MeinUnittest2.py hinzugefügt

---
+++ SW/11_Unittest/Mein_Unittest2.py
@@ -0,0 +1,24 @@
+# Modul für den Unittest importieren 
+import unittest
+
+# Modul mit Zugriff auf die Variable sys.path importieren
+import sys
+
+# Pfad der zu testenden Funktionen anhängen
+sys.path.append("mein Pfad")
+
+# zu testende Funktion importieren
+import meine_funktion
+
+# Testfunktionen definieren
+class Mein_test(unittest.TestCase):
+
+    def test_meine_funktion(self):
+        self.assertEqual(meine_funktion.meine_funktion(mein_input1), mein_output1)
+        self.assertEqual(meine_funktion.meine_funktion(mein_input2), mein_output2)
+        self.assertEqual(meine_funktion.meine_funktion(mein_input3), mein_output3)
+        self.assertEqual(meine_funktion.meine_funktion(mein_input4), mein_output4)
+
+# Unittest ausführen, wenn die Datei direkt aufgerufen wird
+if __name__ == '__main__':
+    unittest.main()
