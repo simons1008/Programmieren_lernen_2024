04d2a3bb6c7ba03a8d530436a47e4944170f75b3
simons1008 <martin.simons@tdmail.de>
2022-11-20 21:10:38 +0100

    Test hinzugefügt

---
+++ SW/Lehrer/billg_strom_unittest.py
@@ -0,0 +1,26 @@
+# Modul für den Unittest importieren 
+import unittest
+
+# Modul mit Zugriff auf die Variable sys.path importieren
+import sys
+
+# Pfad der zu testenden Funktionen anhängen
+sys.path.append("../9_Funktionen")
+
+# zu testende Funktion importieren
+import billig_strom
+
+# Testfunktionen definieren
+class Stromtarif_test(unittest.TestCase):
+
+    def test_billig_strom(self):
+        self.assertAlmostEqual(billig_strom.billig_strom(0), 9.2)
+        self.assertAlmostEqual(billig_strom.billig_strom(10), 17.3)
+        self.assertAlmostEqual(billig_strom.billig_strom(20), 25.4)
+        self.assertAlmostEqual(billig_strom.billig_strom(50), 49.7)
+        self.assertAlmostEqual(billig_strom.billig_strom(100), 90.2)
+        self.assertAlmostEqual(billig_strom.billig_strom(140), 122.6)
+
+# Unittest ausführen, wenn die Datei direkt aufgerufen wird
+if __name__ == '__main__':
+    unittest.main()
