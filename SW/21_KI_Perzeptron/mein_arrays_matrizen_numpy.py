c7f5115fd8f78b19b93e53f26ad491909d7c7507
simons1008 <martin.simons@tdmail.de>
2023-08-04 18:10:54 +0200

    Programm ergänzt -> liesmich.txt entfällt

---
+++ SW/21_KI_Perzeptron/mein_arrays_matrizen_numpy.py
@@ -0,0 +1,33 @@
+# Supervised learning: Erkenne Hund - nicht Hund
+# Daten zum Trainieren des Perzeptrons speichern
+# Quelle:  https://www.youtube.com/@STARTUPTEENS/playlists 
+#          Programmiere mit Python - Baue deine eigene KI! #10
+
+
+# Bibliothek importieren
+import numpy as np
+
+# Dictionary mit Listen
+data = {
+'beine': [4, 4, 4, 4, 4],
+'groesse': [35.4, 28.6, 32.7, 23.9, 45.6],
+'breite': [21.9, 18.7, 16.4, 15.8, 25.6],
+}
+
+# Einen Dictionary-Eintrag ausgeben
+print("Ein Dictionary-Eintrag")
+print(data['groesse'])
+
+# Daten in numpy-Array speichern
+feature = np.array([data['beine'], data['groesse'], data['breite']])
+
+# numpy-Array ausgeben
+print("numpy-Array")
+print(feature)
+
+# Array-Dimensionen ausgeben
+print("Dimensionen")
+print(feature.shape)
+
+# Ende
+input("Fertig? ")
