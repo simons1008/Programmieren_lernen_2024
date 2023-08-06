595b21b3f768a668bd119978a3aebcfca04f7f6f
simons1008 <martin.simons@tdmail.de>
2023-08-06 21:42:16 +0200

    Programm ergänzt

---
+++ SW/Lehrer/save_and_load_a_tensorflow_model.py
@@ -0,0 +1,31 @@
+# save and load a tensorflow model
+# source: https://www.tensorflow.org/guide/keras/serialization_and_saving
+
+import numpy as np
+import tensorflow as tf
+import keras
+
+def get_model():
+    # Create a simple model.
+    inputs = keras.Input(shape=(32,))
+    outputs = keras.layers.Dense(1)(inputs)
+    model = keras.Model(inputs, outputs)
+    model.compile(optimizer=keras.optimizers.Adam(), loss="mean_squared_error")
+    return model
+
+model = get_model()
+# Train the model.
+test_input = np.random.random((128, 32))
+test_target = np.random.random((128, 1))
+model.fit(test_input, test_target)
+
+# Calling `save('my_model.keras')` creates a zip archive `my_model.keras`.
+model.save("my_model.keras")
+
+# It can be used to reconstruct the model identically.
+reconstructed_model = keras.models.load_model("my_model.keras")
+
+# Let's check:
+np.testing.assert_allclose(
+model.predict(test_input), reconstructed_model.predict(test_input)
+)
