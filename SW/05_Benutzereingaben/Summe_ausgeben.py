5f32938234146ecf4094a773e486390ff8ffc237
simons1008 <martin.simons@tdmail.de>
2023-09-09 13:44:04 +0200

    Programme hinzugefügt

---
+++ SW/05_Benutzereingaben/Summe_ausgeben.py
@@ -0,0 +1,12 @@
+# Die Summe der beiden eingegebenen Zahlen ausgeben
+
+# Benutzereingaben anfordern
+zahl1 = input("Gib die erste  Zahl ein ")
+zahl2 = input("Gib die zweite Zahl ein ")
+
+# Strings in Dezimalzahlen umwandeln
+zahl1 = float(zahl1)
+zahl2 = float(zahl2)
+
+# Summe ausgeben
+print("Die Summe der beiden Zahlen ist ", zahl1 + zahl2)
