8deed7306a645dbc5abbbf58ebb49d39137e4faf
simons1008 <martin.simons@tdmail.de>
2024-10-26 14:53:41 +0200

    Aufgaben zur Wiederholung

---
+++ SW/07_Fallunterscheidungen_und_Schleifen/Wiederholung.py
@@ -0,0 +1,31 @@
+#
+# Python - Wiederholung
+#
+
+
+# Erstelle eine Variable
+# - die eine Zahl enthält
+# - die einen Text enthält
+
+
+# Erstelle eine Liste mit 3 Vornamen
+
+
+# Erstelle ein Dictionary mit 3 Einträgen Englisch - Deutsch
+
+
+# Der Computer soll nach deinem Namen fragen und einen Text mit deinem Namen ausgeben
+
+
+# Der Computer soll nach einer Zahl fragen und ausgeben
+# - die Zahl ist größer als 10 
+#     oder 
+# - die Zahl ist kleiner oder gleich 10
+
+
+# Programmiere eine While-Schleife
+# - die zehnmal die Zahl 10 ausgibt
+# - die die Zahlen 1 bis 10 ausgibt
+
+
+
