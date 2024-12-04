0126fe49a97a9659832184fdab48442a9e8bb8cf
simons1008 <martin.simons@tdmail.de>
2024-12-04 18:07:22 +0100

    Programm geändert

--- SW/10_Funktionen_konstruieren/Mein_internet_tarif_kosten.py
+++ SW/10_Funktionen_konstruieren/Mein_internet_tarif_kosten.py
@@ -19,18 +19,27 @@ def volumentarif(verbrauch: list[float]) -> float:
     return kosten
 
 # Input und Output initialisieren
-monate = ["Mai", "Juni", "Juli"]
-mein_verbrauch = [1.2, 1.5, 2]
+monate_liste = [1, 2, 3, 4, 5, 6]
+verbrauch_liste = [1.2, 1.5, 2, 1.2, 1, 2]
 kosten1 = []
 kosten2 = []
 
 # Output erstellen
-for i in range(1,4):
-    kosten1.append(flatrate(i))
-    kosten2.append(volumentarif(mein_verbrauch[0:i]))
-
+for i in range(len(monate_liste)):
+    # Input der Funktion flatrate ist eine Zahl
+    kosten1.append(flatrate(monate_liste[i]))
+    # Input der Funktion volumentarif ist eine Liste 
+    kosten2.append(volumentarif(verbrauch_liste[0:i+1]))
 
 # Ergebnisse drucken
-print("Bis Monat  Flatrate  Volumentarif") 
-for i in range(3):
-    print("{:>9s} {:9.2f} {:9.2f}".format(monate[i], kosten1[i], kosten2[i]))
+print("Flatrate \t Volumentarif") 
+for i in range(len(monate_liste)):
+    print(kosten1[i], "\t\t", kosten2[i])
+
+# Bibliothek importieren
+import matplotlib.pyplot as plt
+
+# Ergebnisse plotten
+plt.plot(monate_liste, kosten1)
+plt.plot(monate_liste, kosten2)
+plt.show()
