dca5b9608ad30fe0d6f7c29aca8ef4fece5cff68
simons1008 <martin.simons@tdmail.de>
2024-12-08 18:42:04 +0100

    Funktionen verschoben

--- SW/09_Funktionen_Input_und_Output/volumentarif.py
+++
@@ -1,9 +0,0 @@
-# Kosten für Volumentarif berechen
-# Die Kosten werden durch das Datenvolumen im Monat bestimmt
-
-# Funktion mit Datentyp
-def volumentarif(verbrauch: list[float]) -> float:
-    kosten = 0
-    for i in range(len(verbrauch)):
-        kosten += verbrauch[i] * 6.5
-    return kosten
