24aaaeeeb1a2d86f5bafe47f7c62fac1d7d39442
simons1008 <martin.simons@tdmail.de>
2025-01-18 12:51:17 +0100

    Hilfsfunktionen schalten die Farben der Ampel

--- SW/15_Zustandsmaschine/Ampel_mit_Grafik.py
+++ SW/15_Zustandsmaschine/Ampel_mit_Grafik.py
@@ -2,10 +2,9 @@
 # Deshalb muss das Programm von der Eingabeaufforderung aus gestartet werden
 
 # Zustandsautomat zur Steuerung einer Ampel
-# Taste1 schaltet GELB_BLINKEN - ROT - ROT_GELB - GRUEN - GELB - ROT
-# Taste2 schaltet die Ampel auf GELB_BLINKEN
+# Die Timer schalten ein Mal GELB_BLINKEN - ROT
+# Danach periodisch - ROT_GELB - GRUEN - GELB - ROT- ROT_GELB - GRUEN usw.
 # Einsatz der Bibliothek statemachine
-# Für die periodische Ausführung: Zeilen 32 bis 34 und 104 bis 108 aktivieren
 
 # Bibliothek für den Zustandsautomaten
 from statemachine import *
@@ -21,7 +20,7 @@ import tkinter as tk
 root = tk.Tk()
 root.geometry("120x240")
 
-# Bildausgabe erzeugen
+# Label mit Ampelfarben erzeugen
 bild_schwarz = tk.PhotoImage(file="schwarz.png")
 label_schwarz_oben = tk.Label(root, image = bild_schwarz)
 label_schwarz_mitte = tk.Label(root, image = bild_schwarz)
@@ -33,24 +32,56 @@ label_rot = tk.Label(root, image = bild_rot)
 bild_gruen = tk.PhotoImage(file="gruen.png")
 label_gruen = tk.Label(root, image = bild_gruen)
 
-# Alle Label in GUI Elemente einbetten
+# Schwarze Label in GUI Elemente einbetten
 label_schwarz_oben.grid(row=0, column=0, padx=20) # padx zur Zentrierung 
 label_schwarz_mitte.grid(row=1, column=0, padx=20)
 label_schwarz_unten.grid(row=2, column=0, padx=20)
-label_rot.grid(row=0, column=0, padx=20) 
+
+# Schwarze Label unsichtbar machen
+# Die Label erinnern sich an ihre Position im Fenster 
+label_schwarz_oben.grid_remove() 
+label_schwarz_mitte.grid_remove()
+label_schwarz_unten.grid_remove()
+
+# Bunte Label in GUI Elemente einbetten
+label_rot.grid(row=0, column=0, padx=20) # padx zur Zentrierung
 label_gelb.grid(row=1, column=0, padx=20)
 label_gruen.grid(row=2, column=0, padx=20)
 
-# Funktion macht alle Label unsichtbar 
+# Bunte Label unsichtbar machen
 # Die Label erinnern sich an ihre Position im Fenster 
-def label_unsichtbar_machen():
-    label_schwarz_oben.grid_remove() 
-    label_schwarz_mitte.grid_remove()
-    label_schwarz_unten.grid_remove()
-    label_rot.grid_remove()
-    label_gelb.grid_remove()
-    label_gruen.grid_remove()
+label_rot.grid_remove()
+label_gelb.grid_remove()
+label_gruen.grid_remove()
 
+# Hilfsfunktionen
+# Funktion schaltet Rot
+def schalte_rot(steuerung):
+    if steuerung:
+        label_schwarz_oben.grid_remove()
+        label_rot.grid()
+    else:
+        label_rot.grid_remove()
+        label_schwarz_oben.grid()
+        
+# Funktion schaltet Gelb
+def schalte_gelb(steuerung):
+    if steuerung:
+        label_schwarz_mitte.grid_remove()
+        label_gelb.grid()
+    else:
+        label_gelb.grid_remove()
+        label_schwarz_mitte.grid()
+        
+# Funktion schaltet Gruen
+def schalte_gruen(steuerung):
+    if steuerung:
+        label_schwarz_unten.grid_remove()
+        label_gruen.grid()
+    else:
+        label_gruen.grid_remove()
+        label_schwarz_unten.grid()
+        
 # Erzeuge das Objekt state_machine
 state_machine = StateMachine()
 
@@ -63,67 +94,55 @@ myTimer_2000 = Neotimer(2000)
 myTimer_5000 = Neotimer(5000)
 myTimer_10000 = Neotimer(10000)
 
-# globale Variable
-eingeschaltet = False
-
-# Funktion aufrufen
-label_unsichtbar_machen()
+# globale Variable steuert gelb_blinken
+gelb_ein = True
 
 # Funktionen in den Zuständen
 # Bis zum Zustandswechsel wird die Funktion wiederholt aufgerufen
 # Einmaliger Aufruf durch Abfrage von state_machine.execute_once
 # Ampel blinkt gelb
 def gelb_blinken():
-    global eingeschaltet
+    global gelb_ein
     if state_machine.execute_once:
         print("Die Ampel blinkt gelb")
+        schalte_rot(False)
+        schalte_gelb(False)
+        schalte_gruen(False)
     if myTimer_500.repeat_execution():
-        label_unsichtbar_machen()
-        if eingeschaltet == False:
-            label_schwarz_oben.grid()
-            label_gelb.grid()
-            label_schwarz_unten.grid()
-        else:
-            label_schwarz_oben.grid()
-            label_schwarz_mitte.grid()
-            label_schwarz_unten.grid()
-        eingeschaltet = not eingeschaltet
+        schalte_gelb(gelb_ein)
+        gelb_ein = not gelb_ein
 
 # Die Ampel ist rot
 def rot():
     if state_machine.execute_once:
         print("Die Ampel ist rot")
-        label_unsichtbar_machen()
-        label_rot.grid()
-        label_schwarz_mitte.grid()
-        label_schwarz_unten.grid()
+        schalte_rot(True)
+        schalte_gelb(False)
+        schalte_gruen(False)
 
 # Die Ampel ist rot/gelb
 def rot_gelb():
     if state_machine.execute_once:
         print("Die Ampel ist rot/gelb")
-        label_unsichtbar_machen()
-        label_rot.grid()
-        label_gelb.grid()
-        label_schwarz_unten.grid()
+        schalte_rot(True)
+        schalte_gelb(True)
+        schalte_gruen(False)
 
 # Die Ampel ist grün
 def gruen():
     if state_machine.execute_once:
         print("Die Ampel ist grün")
-        label_unsichtbar_machen()
-        label_schwarz_oben.grid()
-        label_schwarz_mitte.grid()
-        label_gruen.grid()
+        schalte_rot(False)
+        schalte_gelb(False)
+        schalte_gruen(True)
 
 # Die Ampel ist gelb
 def gelb():
     if state_machine.execute_once:
         print("Die Ampel ist gelb")
-        label_unsichtbar_machen()
-        label_schwarz_oben.grid()
-        label_gelb.grid()
-        label_schwarz_unten.grid()
+        schalte_rot(False)
+        schalte_gelb(True)
+        schalte_gruen(False)
 
 # Funktion ruft state_machine.run() alle 100 ms auf
 def state_machine_loop():
