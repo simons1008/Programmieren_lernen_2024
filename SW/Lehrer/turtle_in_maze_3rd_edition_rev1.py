267bb4880c228c82fb16b9235ecfb3cc885fcb0e
simons1008 <martin.simons@tdmail.de>
2025-05-08 19:06:19 +0200

    turtle_in_maze_3rd_edition.py vereinfacht

---
+++ SW/Lehrer/turtle_in_maze_3rd_edition_rev1.py
@@ -0,0 +1,147 @@
+# Quelle: https://runestone.academy/ns/books/published/pythonds3/Recursion/ExploringaMaze.html
+# Geändert: setworldcoordinates mit Ursprung in der linken unteren Ecke
+#           self.x_translate und self.y_translate entfallen
+#           neue Funktion transform(self, row)
+#           draw_centered_box ersetzt durch draw_box
+#           move_turtle bewegt Turtle in die Mitte der Box
+
+import turtle
+
+START = "S"
+OBSTACLE = "+"
+TRIED = "."
+DEAD_END = "-"
+PART_OF_PATH = "O"
+
+
+class Maze:
+    def __init__(self, maze_filename):
+        with open(maze_filename, "r") as maze_file:
+            self.maze_list = [
+                [ch for ch in line.rstrip("\n")]
+                for line in maze_file.readlines()
+            ]
+        self.rows_in_maze = len(self.maze_list)
+        self.columns_in_maze = len(self.maze_list[0])
+        for row_idx, row in enumerate(self.maze_list):
+            if START in row:
+                self.start_row = row_idx
+                self.start_col = row.index(START)
+                break
+
+        self.y_translate = self.rows_in_maze - 1
+        self.t = turtle.Turtle()
+        self.t.shape("turtle")
+        self.wn = turtle.Screen()
+        self.wn.setworldcoordinates(0, 0, self.columns_in_maze, self.rows_in_maze)
+  
+    def transform(self, row):
+        # rows from top to bottom <-> y coordinates from bottom to top 
+        return (self.rows_in_maze - 1) - row
+
+    def draw_maze(self):
+        self.t.speed(10)
+        self.wn.tracer(0)
+        for row in range(self.rows_in_maze):
+            for col in range(self.columns_in_maze):
+                if self.maze_list[row][col] == OBSTACLE:
+                    self.draw_box(
+                        col, self.transform(row), "orange"
+                    )
+        self.t.color("black")
+        self.t.fillcolor("blue")
+        self.wn.update()
+        self.wn.tracer(1)
+
+    def draw_box(self, x, y, color):
+        self.t.up()
+        self.t.goto(x, y)
+        self.t.color(color)
+        self.t.fillcolor(color)
+        self.t.setheading(90)
+        self.t.down()
+        self.t.begin_fill()
+        for i in range(4):
+            self.t.forward(1)
+            self.t.right(90)
+        self.t.end_fill()
+
+    def move_turtle(self, x, y):
+        self.t.up()
+        x += 0.5
+        y += 0.5
+        self.t.setheading(self.t.towards(x, y))
+        self.t.goto(x, y)
+
+    def drop_bread_crumb(self, color):
+        self.t.dot(10, color)
+
+    def update_position(self, row, col, val=None):
+        if val:
+            self.maze_list[row][col] = val
+        self.move_turtle(col, self.transform(row))
+
+        if val == PART_OF_PATH:
+            color = "green"
+        elif val == OBSTACLE:
+            color = "red"
+        elif val == TRIED:
+            color = "black"
+        elif val == DEAD_END:
+            color = "red"
+        else:
+            color = None
+
+        if color:
+            self.drop_bread_crumb(color)
+
+    def is_exit(self, row, col):
+        return (
+            row == 0
+            or row == self.rows_in_maze - 1
+            or col == 0
+            or col == self.columns_in_maze - 1
+        )
+
+    def __getitem__(self, idx):
+        return self.maze_list[idx]
+
+
+def search_from(maze, start_row, start_column):
+    # try each of four directions from this point until we find a way out.
+    # base Case return values:
+    #  1. We have run into an obstacle, return false
+    maze.update_position(start_row, start_column)
+    if maze[start_row][start_column] == OBSTACLE:
+        return False
+    #  2. We have found a square that has already been explored
+    if (
+        maze[start_row][start_column] == TRIED
+        or maze[start_row][start_column] == DEAD_END
+    ):
+        return False
+    # 3. We have found an outside edge not occupied by an obstacle
+    if maze.is_exit(start_row, start_column):
+        maze.update_position(start_row, start_column, PART_OF_PATH)
+        return True
+    maze.update_position(start_row, start_column, TRIED)
+    # Otherwise, use logical short circuiting to try each direction
+    # in turn (if needed)
+    found = (
+        search_from(maze, start_row - 1, start_column)
+        or search_from(maze, start_row + 1, start_column)
+        or search_from(maze, start_row, start_column - 1)
+        or search_from(maze, start_row, start_column + 1)
+    )
+    if found:
+        maze.update_position(start_row, start_column, PART_OF_PATH)
+    else:
+        maze.update_position(start_row, start_column, DEAD_END)
+    return found
+
+
+my_maze = Maze("maze2.txt")
+my_maze.draw_maze()
+my_maze.update_position(my_maze.start_row, my_maze.start_col)
+
+search_from(my_maze, my_maze.start_row, my_maze.start_col)
